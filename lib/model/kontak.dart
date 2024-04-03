088c205748f0c7c02552b7a8a693b584770b2f90
Siti Marfungah <siti@gmail.com>
2024-04-04 03:04:01 +0700

    Membuat konstruktor untuk membuat objek kontak

--- lib/model/kontak.dart
+++ lib/model/kontak.dart
@@ -4,4 +4,13 @@ class Kontak{
   final String alamat;
   final String noTelepon;
   final String foto;
+
+  Kontak({
+    required this.nama,
+    required this.email,
+    required this.alamat,
+    required this.noTelepon,
+    required this.foto,
+  });
+
 }
