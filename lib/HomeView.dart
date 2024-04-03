7fc05cb3b5190c222d18e950302f4cd8f65aa805
Siti Marfungah <siti@gmail.com>
2024-04-04 04:58:51 +0700

    membuat UI untuk menampilkan daftar kontak

--- lib/HomeView.dart
+++ lib/HomeView.dart
@@ -1,4 +1,5 @@
 import 'package:data_kontak/controller/kontak_controller.dart';
+import 'package:data_kontak/model/kontak.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -19,6 +20,34 @@ class _HomeViewState extends State<HomeView> {
   
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Daftar Orang"),
+      ),
+      body: FutureBuilder<List<Kontak>>(
+        future: _controller.getKontak(), 
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator()); 
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}")); 
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                Kontak kontak = snapshot.data![index];
+                return ListTile(
+                  title: Text(kontak.nama),
+                  subtitle: Text(kontak.email),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(kontak.foto),
+                  ),
+                );
+              },
+            ); 
+          }
+        },
+      ),
+    );
   }
 }
